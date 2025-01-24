b99e4ddeabe0bc86ed553c50f1f8a81246d9a4f1
jealma929 <jealmaformacion@gmail.com>
2025-01-24 12:27:59 +0100

    primera version

---
+++ Ejercicio5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float prec, prod;
+    int cant;
+    /* code */
+    printf("Introduce el precio");
+    scanf("%d%d",&prec);
+    printf("Introduce la cantidad");
+    scanf("%d%d",&cant);
+    prod=(float)prec*cant
+    printf("el importe total es )", n1, " * ", n2, ") = ", prod);
+    return 0;
+}
