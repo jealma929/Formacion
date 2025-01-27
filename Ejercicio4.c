2f299864310324f2c1ecf29ab4b6f87a7169a0a7
jealma929 <jealmaformacion@gmail.com>
2025-01-27 12:59:59 +0100

    27022025

--- Ejercicio4.c
+++ Ejercicio4.c
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-    int n1, n2, n3, n4, sum, prom;
+    int n1, n2, n3, n4, sum;
+    float prom;
     /* code */
-    printf("Introduce cuatro numeros separados por espacios");
-    scanf("%d%d",&n1,&n2,&n3,&n4);
+    printf("\nIntroduce cuatro numeros\n");
+    scanf("%d\n%d\n%d\n%d",&n1,&n2,&n3,&n4);
     sum=n1+n2+n3+n4;
-    prom=sum/4;
-    printf("la suma de ", n1, " + ", n2, " es ", sum);
-    printf("el producto de ", n3, " + ", n4, " es ", prod);
+    prom=(float)sum/4;
+    printf("la suma de %d, %d, %d, %d es %d\n",n1,n2,n3,n4,sum);
+    printf("el promedio de %d, %d, %d, %d es %f",n1,n2,n3,n4,prom);
     return 0;
 }
