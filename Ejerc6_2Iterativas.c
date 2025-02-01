794b54d4b48aa02e48a4f2ee3c9981498df2c124
jealma929 <jealmaformacion@gmail.com>
2025-02-01 18:46:11 +0100

    01022025 version sin final fijo

---
+++ Ejerc6_2Iterativas.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+// escribe los multiplos de 3 emtre el 1-50
+int main(void)
+{
+    int i, num,cant;
+    i=1;
+    do 
+    {
+        printf("\nIntroduce el limite de los multiplos ");
+        scanf(" %d",&cant);
+        if (cant<0)
+            {
+                printf("El numero introducido es erroneo");
+            }
+    } 
+    while (cant<0);
+    printf("\nLos multiplos de 3 entre 1 y %d son: \n",cant);
+    do
+    {
+    num=i*3;
+    printf("%d ",num);
+    i++;
+    } while (i*3<=cant);
+    return 0;
+}
