bb0a0dd08c82ce35588ac8e18ff2d32f6e6d8785
jealma929 <jealmaformacion@gmail.com>
2025-02-01 19:13:20 +0100

    01022025 definitivo

---
+++ Ejerc7Iterativas.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+// escribe los multiplos de 3 emtre el 1-50
+int main(void)
+{
+    int i, cant;
+    i=1;
+    do 
+    {
+        printf("\nIntroduce el numero para hacer la tabla de multipicar ");
+        scanf(" %d",&cant);
+        if (cant<0 || cant>10)
+            {
+                printf("El numero introducido es erroneo");
+            }
+    } 
+    while (cant<0 || cant>10);
+    printf("\nLa tabla de multiplicar del numero %d es \n",cant);
+    for (i=1;i<=10;i++)
+    {
+        //prod=num*i;
+        printf("%d x %d = %d\n",cant,i,cant*i);
+    }
+    return 0;
+}
