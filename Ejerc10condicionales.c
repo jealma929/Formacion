dac798a92948e3ac7199ab53aab8a50a8742dcf5
jealma929 <jealmaformacion@gmail.com>
2025-02-03 12:35:13 +0100

    03022025 definitivo

--- Ejerc10condicionales.c
+++ Ejerc10condicionales.c
@@ -12,26 +12,15 @@ int main(void)
     printf("\nDame el tercer numero ");
     scanf("%d",&num3);
 
-    if (num1<num2)
-        {
-            paso++;
-        }     
-    if(num2<num3)
-        {
-            paso++
-        }
-          
-
-    if (paso==2)
+    if (num1>num2 && num2>num3)
     {
         printf("\nSe han introducido de forma decreciente");
     }
-    else 
-    {
-        printf("\n %d",num1);
-        printf("\n %d",num2);
-        printf("\n %d",num3);
-    }
+    else
+        {
+            printf("\nSe han introducido de forma aleatoria");
+        }
+    
     return 0;
 
 }
