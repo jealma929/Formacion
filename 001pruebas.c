dccfd154272bdb51c0353e30c57b75254a4ec0c2
jealma929 <jealmaformacion@gmail.com>
2025-02-05 13:03:48 +0100

    05022025

--- 001pruebas.c
+++ 001pruebas.c
@@ -3,10 +3,15 @@
 //prueba switch case
 int main(void)
 {
-    int i, num, cont, base, exp;
+    int i,*p ,num, cont, base, exp,bisi=28;
     char opcion;
     i=0;cont=0;
+    p=&bisi;
 
    printf("\n modulo %d", 0%2);
+   while(1){
+                printf("%d",*p);
+            }
    printf("\n cociente %d", 0/2);
+   printf("\n bisiesto %d",*p); 
 }
--- array_cadenas_punteros_C/Ejerc6.2array.c
+++ array_cadenas_punteros_C/Ejerc6.2array.c
@@ -5,30 +5,27 @@ int main(void)
 
     int year[12];
     int i,bisi,nyear,*p;
-
+//pedimos el año
     printf("\nIntroduce el año ");
-    scanf("%d ",nyear);
+    scanf("%d",&nyear);
 
-    if (nyear%100==0 && nyear%400==0)
+    if ((nyear%100==0) && (nyear%400==0))
         {
             bisi=29;
         }
         else 
                 {
-                    if (nyear%100==0 && nyear%400==0)
+                    if (nyear%4==0)
                         {
-                            bisi=29;
+                            bisi=29;               
                         }
+                        else
+                            {
+                                bisi=28;
+                            }
                 }
-                
-        }
-        else
-            {
-                bisi=28;
-            }
+// asignamos los datos a la tabla
 
-
-    p=year;
     for (i=1;i<=12;i++)
         {
             if (i==1||i==3||i==5||i==7||i==8||i==10||i==12)
@@ -54,7 +51,7 @@ int main(void)
                         } 
                 }
                 
-                
+ //imprimimos los meses               
 
         }
     for (i=0;i<12;i++)
@@ -62,5 +59,5 @@ int main(void)
             p=&year[i];
             printf("\nEl mes %d tiene %d dias.",i+1,*p);
         }
-return 0;
+    return 0;
 }
