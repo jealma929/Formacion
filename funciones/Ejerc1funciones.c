44608c4baed4fe8818eaf66916046707a9e82835
jealma929 <jealmaformacion@gmail.com>
2025-02-07 14:14:41 +0100

    07022025 definitivo

--- funciones/Ejerc1funciones.c
+++ funciones/Ejerc1funciones.c
@@ -2,35 +2,34 @@
 #include<stdlib.h>
 #include<time.h>
 
-/*
+
 int mayornum(int *tabla,int t)
 {
-    int i=0,m=0;
-    printf("\n");
-        do
-    {
-        if (tabla[i]<tabla[i+1]){m=tabla[i+1];}
-        else {m=tabla[i];}
-        i++;
-        printf("%d ",m);
-    }
+    int i=1,num=0;
+    num=tabla[0];
+    //printf("\n");
+    do
+        {
+            if (num<tabla[i]){num=tabla[i];}
+            i++;
+            //printf("%d ",num);
+        }
     while(i!=t);
-    return(m);
-
-}*/
+    return(num);
+}
 
 void mayor(int *tabla,int t,int **p)
 {
-    int i=0;
-    int *m;
-    m=*p;
+    int i=1, num;
+    int *m=&tabla[0];
+
+    num=tabla[0];
+    //printf("\n");
         do
         {
-            if (tabla[i]<tabla[i+1]){m=&tabla[i+1];}
-            else {m=&tabla[i];}  
+            if (num<tabla[i]){m=&tabla[i];num=tabla[i];}
             i++;
-            printf("\n");
-            printf("%d, ",m);
+            //printf("%d-%d-%d  ",i,tabla[i],num);
         }
         while(i!=t);
    *p=m;
@@ -41,7 +40,7 @@ int main(void)
 {
     srand(time(NULL));
     int tabla[10];
-    int i,m; 
+    int i,may=0; 
     int *max;
     max=tabla;
 
@@ -50,11 +49,11 @@ int main(void)
         tabla[i]=0+rand()%19;
         printf("%d ",tabla[i]);
     }
-    //m=mayornum (tabla,10);
+    may=mayornum (tabla,10);
 
     mayor(tabla,10,&max);
-
-    printf("\nEl numero mayor es %d  %d",m,*max);
+    
+    printf("\nEl numero mayor es %d  %d",may,*max);
 
     return 0;
 }
